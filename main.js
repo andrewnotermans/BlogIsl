67e55d9faf43768d20555bd20002c197cf47c853
Andrew Notermans <andrewnotermans@gmail.com>
2023-04-12 13:51:48 +0200

    commit 'restore all'

---
+++ main.js
@@ -0,0 +1,46 @@
+//menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-menu__item__link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+//slide carousel 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("custom-slider");
+  if (window.matchMedia("(max-width:568px)").matches){
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+  
+  slides[slideIndex-1].style.display = "block";  
+} else{
+    stop;
+}
+  
+}
